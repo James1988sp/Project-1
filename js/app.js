bb86f06d75c8470b3f9bfbb9635611ce5a38c80b
James1988sp <gianmariacarrodano@libero.it>
2017-02-10 17:06:18 +0000

    First step

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-console.log('J')
+console.log('Js loaded');
+
+const operator = ['+', '-', '/', '*'];
+const firstNumber = Math.ceil(Math.random() * 10);
+const secondNumber = Math.ceil(Math.random() * 10);
+operator = Math.form(Math.random() + operator.length);
